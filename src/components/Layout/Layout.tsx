7e1f3e3e9b4da6150ad15e85034fba3966065de2
Francis Lagueu <lagson6@gmail.com>
2023-02-05 13:16:51 -0800

    Added navbar and footer

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Navbar from '../Navbar/Navbar';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+export default Layout;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,56 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import React from 'react';
+
+type NavbarProps = {};
+type NavItemProps = {
+  children: React.ReactNode;
+  isLast?: boolean;
+  to: string;
+};
+
+const NavItem: React.FC<NavItemProps> = ({ children, isLast, to = '/' }) => {
+  return (
+    <Text
+      mb={{ base: isLast ? 0 : 8, sm: 0 }}
+      mr={{ base: 0, sm: isLast ? 0 : 8 }}
+      display='block'
+    >
+      <Link href={to}>{children}</Link>
+    </Text>
+  );
+};
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <Flex
+      bg='white'
+      height='64px'
+      padding='6px 16px'
+      justifyContent='space-between'
+      align='center'
+      wrap='wrap'
+      as='nav'
+    >
+      <Box>
+        <Text fontSize='lg' fontWeight='bold'>
+          Pledgerville
+        </Text>
+      </Box>
+      <Box flexBasis={{ base: '100%', md: 'auto' }}>
+        <Flex
+          align='center'
+          justify={['center', 'space-between', 'flex-end', 'flex-end']}
+          direction={['column', 'row', 'row', 'row']}
+          pt={[4, 4, 0, 0]}
+        >
+          <NavItem to='/'>Home</NavItem>
+          <NavItem to='/services'>Services</NavItem>
+          <NavItem to='/about-us'>About Us</NavItem>
+          <NavItem to='/contact-us'>Contact Us</NavItem>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
+export default Navbar;
