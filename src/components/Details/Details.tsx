f8f30f29acb30fae97c4b7b4d9953d819588f921
Francis Lagueu <lagson6@gmail.com>
2023-05-15 16:12:20 -0700

    Removed the box shadow and added background color of white to the details section

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -29,31 +29,37 @@ const Detail: React.FC<DetailProps> = ({ imageUrl, title, description }) => {
     transform: inView ? 'translateY(0)' : 'translateY(50px)',
   });
   return (
-    <SimpleGrid m={6} columns={{ base: 1, md: 2 }} spacing={11}>
+    <SimpleGrid
+      m={6}
+      columns={{ base: 1, md: 2 }}
+      spacing={11}
+      backgroundColor={'white'}
+      borderRadius='xl'
+    >
       <Flex flexGrow={1}>
         <Box
-          bg="white"
-          borderRadius="2xl"
-          border="solid white"
-          boxShadow="dark-lg"
+          bg='white'
+          borderRadius='xl'
+          border='solid white'
+          boxShadow='dark-sm'
           p={0}
           ref={ref}
           style={{ overflow: 'hidden' }}
         >
           <animated.div style={animationProps}>
-          <Image src={imageUrl} alt={title} width="430px" height="320px" />
+            <Image src={imageUrl} alt={title} width='430px' height='320px' />
           </animated.div>
         </Box>
       </Flex>
       <Flex>
-        <Stack mt="1" spacing="2">
+        <Stack mt='1' spacing='2' mr='1'>
           <animated.div style={animationProps}>
-            <Heading fontFamily="Linotype Didot" fontSize="2xl" size="md">
+            <Heading fontFamily='Linotype Didot' fontSize='2xl' size='md'>
               {title}
             </Heading>
           </animated.div>
           <animated.div style={animationProps}>
-            <Text fontFamily="Linotype Didot" fontSize="xl">
+            <Text fontFamily='Linotype Didot' fontSize='xl'>
               {description}
             </Text>
           </animated.div>
@@ -69,28 +75,25 @@ const Details: React.FC = () => {
       imageUrl: '/computers_use.jpg',
       title: 'LIFESTYLE & SUBSTANCE',
       description:
-       'Our Pacoima community provides the ideal balance of privacy and community interaction, allowing residents to maintain their independence while having access to engaging activities and social events. The facility also offers various amenities and residents have the freedom to choose from a variety of villa sizes and layouts equipped with appliances and conveniences, allowing them to live comfortably and elegantly.'
-        
+        'Our Pacoima community provides the ideal balance of privacy and community interaction, allowing residents to maintain their independence while having access to engaging activities and social events. The facility also offers various amenities and residents have the freedom to choose from a variety of villa sizes and layouts equipped with appliances and conveniences, allowing them to live comfortably and elegantly.',
     },
     {
       imageUrl: '/hand-shake.jpg',
       title: 'OUR COMMITMENT',
       description:
-      'Our mission at Pledgerville is to foster a sense of purpose and independence in our residents lives. We understand that we are not just a community, but also a part of the larger society. As such, we are committed to making a positive impact in the local community by creating employment opportunities for the locals and supporting small businesses. We believe that by investing in the community, we can foster a sense of purpose and belonging that enhances the quality of life for everyone.'
-      
+        'Our mission at Pledgerville is to foster a sense of purpose and independence in our residents lives. We understand that we are not just a community, but also a part of the larger society. As such, we are committed to making a positive impact in the local community by creating employment opportunities for the locals and supporting small businesses. We believe that by investing in the community, we can foster a sense of purpose and belonging that enhances the quality of life for everyone.',
     },
     {
       imageUrl: '/branch-dof.jpg',
       title: 'PEACEFUL & SATISFYING',
       description:
-      'Our independent living facility is the epitome of tranquility and contentment. We pride ourselves on creating an environment that promotes peacefulness and fulfillment, where our residents can lead a life of serenity and harmony. Our serene surroundings provide the perfect atmosphere for reflection and contemplation, with ample opportunities to connect with nature and enjoy the great outdoors.',
-      
+        'Our independent living facility is the epitome of tranquility and contentment. We pride ourselves on creating an environment that promotes peacefulness and fulfillment, where our residents can lead a life of serenity and harmony. Our serene surroundings provide the perfect atmosphere for reflection and contemplation, with ample opportunities to connect with nature and enjoy the great outdoors.',
     },
   ];
 
   return (
-    <Container maxW="container.lg" mt={10} centerContent overflow="hidden">
-      <Box flexDirection="column" gap={8}>
+    <Container maxW='container.lg' mt={10} centerContent overflow='hidden'>
+      <Box flexDirection='column' gap={8}>
         {details.map((detail, index) => (
           <Detail
             key={index}
--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -1,4 +1,12 @@
-import { Container, Box,  Flex, Heading, SimpleGrid, Text, Image } from '@chakra-ui/react';
+import {
+  Container,
+  Box,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Text,
+  Image,
+} from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
@@ -7,7 +15,10 @@ const Feature: React.FC = () => {
 
   const containerVariants = {
     hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { staggerChildren: 0.2, delayChildren: 0.2 } },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.2, delayChildren: 0.2 },
+    },
   };
 
   const itemVariants = {
@@ -16,49 +27,119 @@ const Feature: React.FC = () => {
   };
 
   return (
-    <motion.div ref={ref} variants={containerVariants} initial="hidden" animate={inView ? 'visible' : 'hidden'}>
-      <Container maxW="8xl" paddingLeft={200} paddingBottom={20} paddingTop={40}>
+    <motion.div
+      ref={ref}
+      variants={containerVariants}
+      initial='hidden'
+      animate={inView ? 'visible' : 'hidden'}
+    >
+      <Container
+        maxW='8xl'
+        paddingLeft={200}
+        paddingBottom={20}
+        paddingTop={40}
+      >
         <SimpleGrid columns={{ base: 1, md: 1 }} spacing={50}>
-          <Flex direction="row" gap={5}>
-            <Flex direction="column" w="full" h="full" alignItems="stretch" flexGrow={1} gap={3}>
+          <Flex direction='row' gap={5}>
+            <Flex
+              direction='column'
+              w='full'
+              h='full'
+              alignItems='stretch'
+              flexGrow={1}
+              gap={3}
+            >
               <motion.div variants={itemVariants}>
-              <Box bg="white" boxShadow="dark-lg" border="solid white"  borderRadius="2xl" overflow="hidden"  >
-                  <Image src="/black-man-cooking.jpg" alt="Black man cooking" objectFit="cover" width="600px" height="400px" />
+                <Box
+                  bg='white'
+                  boxShadow='dark-sm'
+                  borderRadius='xl'
+                  overflow='hidden'
+                >
+                  <Image
+                    src='/black-man-cooking.jpg'
+                    alt='Black man cooking'
+                    objectFit='cover'
+                    width='600px'
+                    height='400px'
+                  />
                 </Box>
               </motion.div>
               <motion.div variants={itemVariants}>
-              <Box bg="white" boxShadow="dark-lg"  border="solid white" borderRadius="2xl" overflow="hidden">
-                  <Image src="/computers.jpg" alt="Black man cooking" objectFit="cover" width="600px" height="400px"  />
+                <Box
+                  bg='white'
+                  boxShadow='dark-sm'
+                  borderRadius='xl'
+                  overflow='hidden'
+                >
+                  <Image
+                    src='/computers.jpg'
+                    alt='Black man cooking'
+                    objectFit='cover'
+                    width='600px'
+                    height='400px'
+                  />
                 </Box>
               </motion.div>
             </Flex>
-            <Flex direction="column" gap={-30} maxH="390px">
+            <Flex direction='column' gap={-30} maxH='390px'>
               <motion.div variants={itemVariants}>
-                <Heading fontSize="normal" fontWeight="bold" noOfLines={1}>
-                  {/* Add your heading text here */}
+                <Heading
+                  fontSize='5xl'
+                  fontFamily='Linotype Didot'
+                  noOfLines={1}
+                  paddingLeft={140}
+                  paddingBottom={34}
+                >
+                  Home Is Here
                 </Heading>
               </motion.div>
               <motion.div variants={itemVariants}>
-                <Heading  fontSize="5xl" fontFamily="Linotype Didot" noOfLines={1} paddingLeft={140} paddingBottom={34} >Home Is Here</Heading>
-              </motion.div>
-              <motion.div variants={itemVariants}>
-                <Text  fontFamily="Linotype Didot" fontSize="xl" style={{ wordWrap: 'break-word'} } paddingTop={1}>
-                  Our senior community is the perfect place to benefit from round-the-clock<br/> 
-                  support and enjoy all the advantages of a vibrant lifestyle. As a resident of <br/>
-                  our independent living community, you can look forward to taking part in<br/> 
-                  exciting community events including group exercises, game and movie night<br/> 
-                  or morning coffee in our Great Room.  Whether you desire to forge new <br/>
-                  relationships, explore new hobbies, or simply unwind and de-stress, our senior<br/> 
+                <Text
+                  fontFamily='Linotype Didot'
+                  fontSize='xl'
+                  style={{ wordWrap: 'break-word' }}
+                  paddingTop={1}
+                >
+                  Our senior community is the perfect place to benefit from
+                  round-the-clock
+                  <br />
+                  support and enjoy all the advantages of a vibrant lifestyle.
+                  As a resident of <br />
+                  our independent living community, you can look forward to
+                  taking part in
+                  <br />
+                  exciting community events including group exercises, game and
+                  movie night
+                  <br />
+                  or morning coffee in our Great Room. Whether you desire to
+                  forge new <br />
+                  relationships, explore new hobbies, or simply unwind and
+                  de-stress, our senior
+                  <br />
                   community is the perfect haven for you or your loved one.
                 </Text>
               </motion.div>
               <motion.div variants={itemVariants}>
-                <Text fontFamily="Linotype Didot" fontSize="xl" paddingTop={200} >
-                  In contemporary times, computer proficiency is becoming vital for seniors<br/>
-                  to navigate their daily routines. It is crucial for them to possess a thorough<br/>
-                  comprehension of computers and software applications to communicate with <br/>
-                  essential services, organizations, physicians, and relatives. Personal computers <br/>
-                  and computer literacy greatly improve and simplify the residents' search for financial autonomy while also offering prospects for education, socializing, and community-related information.
+                <Text
+                  fontFamily='Linotype Didot'
+                  fontSize='xl'
+                  paddingTop={200}
+                >
+                  In contemporary times, computer proficiency is becoming vital
+                  for seniors
+                  <br />
+                  to navigate their daily routines. It is crucial for them to
+                  possess a thorough
+                  <br />
+                  comprehension of computers and software applications to
+                  communicate with <br />
+                  essential services, organizations, physicians, and relatives.
+                  Personal computers <br />
+                  and computer literacy greatly improve and simplify the
+                  residents' search for financial autonomy while also offering
+                  prospects for education, socializing, and community-related
+                  information.
                 </Text>
               </motion.div>
             </Flex>
