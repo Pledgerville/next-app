5fd7556a7109e4e3fa42e3d406ee42be11b87929
Tbyrd02 <26thour@gmail.com>
2023-05-30 13:59:43 -0700

    add scroll to events as number of events grow

--- src/pages/featured_events.tsx
+++ src/pages/featured_events.tsx
@@ -7,16 +7,45 @@ const FeaturedEvents = () => {
     {
       id: 1,
       title: 'Meditation with Nadi',
-      date: '2023-05-29',
+      date: '2023-05-30',
       description: 'Meditation with Nadi is relaxing and fulfilling. Take time to reflect and become one with nature. Slots are limited.',
       image: '/meditation.jpg',
     },
     {
       id: 2,
       title: "Pledgerville's Anniversary",
-      date: '2023-11-25',
+      date: '2023-06-29',
       description: "Come celebrate Pledgerville's Anniversary on November 25th, 2023. Come as you are and bring your glowing selves.",
       image: '/celebration.jpg',
+    },
+
+    {
+      id: 3,
+      title: 'Water Workout',
+      date: '2023-06-19',
+      description: 'Meditation with Nadi is relaxing and fulfilling. Take time to reflect and become one with nature. Slots are limited.',
+      image: '/play-time.jpg',
+    },
+    {
+      id: 4,
+      title: "Tea Time Book Club",
+      date: '2023-6-25',
+      description: "Please join us as we take a visit to the local Mahogany Tea Shop. Bring your favorite mug and book",
+      image: '/computers.jpg',
+    },
+    {
+      id: 5,
+      title: 'Walk & Talk',
+      date: '2023-06-10',
+      description: 'Come take a walk and chat with your fellow residents as we visit local shops.',
+      image: '/hand-bag.jpg',
+    },
+    {
+      id: 6,
+      title: "Church",
+      date: '2023-06-25',
+      description: "Weekly Sunday Service",
+      image: '/mountain_sunrise.jpg',
     },
   ];
 
@@ -36,7 +65,7 @@ const FeaturedEvents = () => {
       <Text fontFamily="Linotype Didot" align="center" mb={4}>
         {view_today}
       </Text>
-      <Box mb={4} maxWidth="300px" mx="auto">
+      <Box mb={4} maxWidth="400px" mx="auto">
         <Input
           borderRadius="xl"
           border="2px solid #ccc"
@@ -47,53 +76,55 @@ const FeaturedEvents = () => {
         />
       </Box>
       <Flex justify="center">
-        <Stack spacing={3} maxWidth="1200px">
-          {filteredEvents.map((event) => (
-            <Flex
-              key={event.id}
-              align="center"
-              mb={2}
-              border="2px solid #ccc"
-              borderRadius="md"
-              p={4}
-              maxWidth="100%"
-              boxShadow="md"
-              position="relative"
-              _before={
-                today === event.date
-                  ? {
-                      content: '""',
-                      position: 'absolute',
-                      top: 0,
-                      left: 0,
-                      bottom: 0,
-                      width: '7px',
-                      bg: '#2036EC',
-                      borderRadius: 'md',
-                    }
-                  : {}
-              }
-              bg="white"
-            >
-              <Box boxShadow="xl" flexShrink={0} mr={4}>
-                <Image
-                  src={event.image}
-                  alt={event.title}
-                  objectFit="cover"
-                  borderRadius="xl"
-                  boxSize="200px"
-                />
-              </Box>
-              <Box>
-                <Text fontSize="xl" fontWeight="bold" mb={2}>
-                  {event.title}
-                </Text>
-                <Text fontFamily="Linotype Didot" fontWeight="slight">Date: {event.date}</Text>
-                <Text fontFamily="Linotype Didot" fontWeight="slight">Description: {event.description}</Text>
-              </Box>
-            </Flex>
-          ))}
-        </Stack>
+        <Box maxHeight="600px" overflow="auto"> {/* Container with scroll */}
+          <Stack spacing={3} maxWidth="1200px">
+            {filteredEvents.map((event) => (
+              <Flex
+                key={event.id}
+                align="center"
+                mb={2}
+                border="2px solid #ccc"
+                borderRadius="md"
+                p={4}
+                maxWidth="100%"
+                boxShadow="md"
+                position="relative"
+                _before={
+                  today === event.date
+                    ? {
+                        content: '""',
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        bottom: 0,
+                        width: '7px',
+                        bg: '#2036EC',
+                        borderRadius: 'md',
+                      }
+                    : {}
+                }
+                bg="white"
+              >
+                <Box boxShadow="xl" flexShrink={0} mr={4}>
+                  <Image
+                    src={event.image}
+                    alt={event.title}
+                    objectFit="cover"
+                    borderRadius="xl"
+                    boxSize="200px"
+                  />
+                </Box>
+                <Box>
+                  <Text fontSize="xl" fontWeight="bold" mb={2}>
+                    {event.title}
+                  </Text>
+                  <Text fontFamily="Linotype Didot" fontWeight="slight">Date: {event.date}</Text>
+                  <Text fontFamily="Linotype Didot" fontWeight="slight">Description: {event.description}</Text>
+                </Box>
+              </Flex>
+            ))}
+          </Stack>
+        </Box>
       </Flex>
     </Box>
   );
